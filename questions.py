de93fe6c18edd865116a20c523f94ca88cc01747
roruingle <rutuingle123@gmail.com>
2024-11-25 10:31:26 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,38 @@
+# cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+# cars.append("ferrari")
+# print(cars)
+
+# cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+# cars.insert(3,"porsche")
+# print(cars)
+
+# cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+# cars.sort()
+# print(cars)
+
+# cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+# cars.remove("audi")
+# print(cars)
+
+# cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+# cars.index("mustang")
+# print(cars.index("mustang"))
+
+# cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+# cars.extend(["tesla","nissan"])
+# print(cars)
+
+# cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+# cars.pop(8)
+# print(cars)
+
+# cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+# cars.count("bmw") 
+# print(cars.count("bmw"))
+
+cars=["bmw","mahindra","suzuki","rolls royce","bentley","aston martin","tata","audi","mustang"]
+cars.appned("jaguar","fiat")
+print(cars)
+
+
+
